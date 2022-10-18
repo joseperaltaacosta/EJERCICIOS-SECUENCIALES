4e53d5c274852eb0c498ea8264aff97896e0f895
Jose <al_jperaltaa@iessierradelasvillas.org>
2022-10-18 14:40:52 +0200

    EJERCICIOS SECUENCIALES DEFINIDOS

---
+++ EJERCICIOS SECUENCIALES CON FUNCIONES.py
@@ -0,0 +1,12 @@
+def ejercicio1():
+    nombre=""
+    nombre =input ("Dime tu nombre: ")
+    print ("Buenos días", nombre)
+
+def ejercicio2():
+    base = 0
+    altura = 0
+    base= (int) (input ("Dime la base del rectángulo:"))
+    altura= (int) (input ("Dime la altura del rectángulo:"))
+    print ("El perímetro del rectángulo es:", (2*base)+(2*altura))
+    print ("El area del rectángulo es:", base*altura)
